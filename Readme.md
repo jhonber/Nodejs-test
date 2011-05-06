bd17ff607e66b7d01c563de519c4b9c7947fe47a
Jhon Jimenez <jhonber1@gmail.com>
2011-05-06 10:41:19 -0500

    Modifico server.js para usar el layout en views

--- Readme.md
+++ Readme.md
@@ -1,6 +1,9 @@
 #Nodejs-test
 
-Repositorio para hacer pruebas con Nodejs
-
-Este es un proyecto abierto para el que quiera participar
+-Repositorio para hacer pruebas con Nodejs
+-Este es un proyecto abierto para el que quiera participar
 y necesite hacer sus pruebas
+
+##Versión de nodejs y express
+-Node v0.4.7
+-Express 2.3.2
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
 /**
  * Module dependencies.
  */
-/*Importo la libreria express(Framework)*/
+/*Importo la libreria express (Framework)*/
 var express = require('express');
 
 /*Se crea el servidor*/
@@ -10,11 +9,9 @@ var app = module.exports = express.createServer();
 
 // Configuration
 app.configure(function(){
-  /*Indicamos que las vistas estan en el directorio /views*/
   app.set('views', __dirname + '/views');
-  /*Indicamos que vamos a usar el motor de plantallas Jade*/
   app.set('view engine', 'jade');
-  /*Se deben usar por buenas practicas*/
+  app.set('view options', {layout: true});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -33,18 +30,12 @@ app.configure('production', function(){
 });
 
 // Routes
-// Se manejas las peticiones a las rutas, deacuerdo a la ruta solicitada
-// una funcion se encarga de manejar cada peticion
+
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'jhonber\'s Site'
+    title: 'HackLab'
   });
 });
-
-/*app.get('/como', function(req, res){
-  res.render('kumokojo/como', {layout:false})
-});
-*/
 
 // Only listen on $ node app.js
 
